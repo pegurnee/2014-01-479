a3e33f22a09ec5cb4c42053cd6606340bf9b34b2
Eddie Gurnee <egurnee@emich.edu>
2014-02-04 20:25:08 -0500

    add iOSCalc assignment

---
+++ Assignments/iOSCalc/iOSCalc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOSCalc
+//
+//  Created by Edward Gurnee on 2/4/14.
+//  Copyright (c) 2014 Eddie Gurnee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
