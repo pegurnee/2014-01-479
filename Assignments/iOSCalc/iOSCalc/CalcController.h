5537fe75563a502b62c9be2978765f61ad5e8672
Eddie Gurnee <egurnee@emich.edu>
2014-02-06 15:18:37 -0500

    add calcController and calcModel to assignement 1

---
+++ Assignments/iOSCalc/iOSCalc/CalcController.h
@@ -0,0 +1,24 @@
+//
+//  CalcController.h
+//  iOSCalc
+//
+//  Created by Edward Gurnee on 2/6/14.
+//  Copyright (c) 2014 Eddie Gurnee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CalcModel.h"
+
+@interface CalcController : UIViewController {
+    
+    
+    
+    CalcModel *calc_model;
+    
+    
+}
+
+
+
+
+@end
---
+++ Assignments/iOSCalc/iOSCalc/CalcController.m
@@ -0,0 +1,13 @@
+//
+//  CalcController.m
+//  iOSCalc
+//
+//  Created by Edward Gurnee on 2/6/14.
+//  Copyright (c) 2014 Eddie Gurnee. All rights reserved.
+//
+
+#import "CalcController.h"
+
+@implementation CalcController
+
+@end
