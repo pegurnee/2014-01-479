8b0d4e4a7fa43da6d2c0dab8fe087a81f77380f0
Eddie Gurnee <egurnee@emich.edu>
2014-02-06 19:44:12 -0500

    include storyboard lecture

---
+++ Lectures/Lecture_02_06_14/ByTheWay/ByTheWay/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ByTheWay
+//
+//  Created by Edward Gurnee on 2/6/14.
+//  Copyright (c) 2014 Eddie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
