6ac6da6685f668fd2e2cd3890b11d92a1aab3789
Eddie Gurnee <egurnee@emich.edu>
2014-02-11 20:38:04 -0500

    include VeryQuickly

---
+++ Lectures/Lecture_02_11_14/VeryQuickly/VeryQuickly/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VeryQuickly
+//
+//  Created by Edward Gurnee on 2/11/14.
+//  Copyright (c) 2014 Eddie Gurnee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Lectures/Lecture_02_11_14/VeryQuickly/VeryQuickly/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VeryQuickly
+//
+//  Created by Edward Gurnee on 2/11/14.
+//  Copyright (c) 2014 Eddie Gurnee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
