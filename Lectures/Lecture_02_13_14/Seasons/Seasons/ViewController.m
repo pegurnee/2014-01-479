ae40497b052b9ab29b99ee6ddf73d74d68ba5ab4
Eddie Gurnee <egurnee@emich.edu>
2014-02-13 20:39:53 -0500

    add Feb13 lecture projects

---
+++ Lectures/Lecture_02_13_14/Seasons/Seasons/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Seasons
+//
+//  Created by Edward Gurnee on 2/13/14.
+//  Copyright (c) 2014 Eddie Gurnee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
