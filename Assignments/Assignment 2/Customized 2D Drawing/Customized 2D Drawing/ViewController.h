df2ea8fdb887c7a96e1ee4644071979463860e31
Eddie Gurnee <egurnee@emich.edu>
2014-02-17 19:33:05 -0500

    renamed assignment 2

---
+++ Assignments/Assignment 2/Customized 2D Drawing/Customized 2D Drawing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Customized 2D Drawing
+//
+//  Created by Edward Gurnee on 2/17/14.
+//  Copyright (c) 2014 Eddie Gurnee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Assignments/Assignment 2/Customized 2D Drawing/Customized 2D Drawing/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Customized 2D Drawing
+//
+//  Created by Edward Gurnee on 2/17/14.
+//  Copyright (c) 2014 Eddie Gurnee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
