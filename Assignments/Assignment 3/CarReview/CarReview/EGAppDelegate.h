31621506025600b7ed0ba8239935f5f86e830c12
Eddie Gurnee <pegurnee@gmail.com>
2014-03-11 14:41:26 -0400

    include assignment 3

---
+++ Assignments/Assignment 3/CarReview/CarReview/EGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EGAppDelegate.h
+//  CarReview
+//
+//  Created by Eddie Gurnee on 3/10/14.
+//  Copyright (c) 2014 Eddie Gurnee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Assignments/Assignment 3/CarReview/CarReview/EGViewController.h
@@ -0,0 +1,13 @@
+//
+//  EGViewController.h
+//  CarReview
+//
+//  Created by Eddie Gurnee on 3/10/14.
+//  Copyright (c) 2014 Eddie Gurnee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EGViewController : UIViewController
+
+@end
