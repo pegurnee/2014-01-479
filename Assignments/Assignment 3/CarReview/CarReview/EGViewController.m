3b33298f7e43604ca131a4141d82feea8cd7fe74
Eddie Gurnee <pegurnee@gmail.com>
2014-03-11 15:21:08 -0400

    being plist testing

--- Assignments/Assignment 3/CarReview/CarReview/EGViewController.m
+++ Assignments/Assignment 3/CarReview/CarReview/EGViewController.m
@@ -24,7 +24,22 @@
     
     // Load the file content and read the data into arrays
     NSDictionary *myDict = [[NSDictionary alloc] initWithContentsOfFile:path];
-    myTableData = [myDict objectForKey:@"MovieTitle"];
+    myTableData = [myDict objectForKey:@"GM"];
+    NSLog(@"hi");
+    for (int i = 0; i < 3; i++) {
+        /*
+        NSString *msg = [[NSString alloc] initWithFormat:@"You've selected %@", [myTableData objectAtIndex: i]];
+        UIAlertView *myAlert = [[UIAlertView alloc] initWithTitle:@"Row Select"
+                                                          message:msg delegate:nil
+                                                cancelButtonTitle:@"OK"
+                                                otherButtonTitles:nil];
+        
+        [myAlert show];
+         */
+        NSLog(@"Object %d: %@", i, [myTableData objectAtIndex: i]);
+    }
+
+    
     myDetailData = [myDict objectForKey:@"MovieRating"];
 }
 
