4f2caed4eeadd8f849e5f134916b0b2cd062a062
Eddie Gurnee <pegurnee@gmail.com>
2014-03-11 21:56:47 -0400

    complete exam question 3

---
+++ Exams/Exam1_EddieGurnee/Ex01 - ToDoList/Ex01 - ToDoList/EGViewController.h
@@ -0,0 +1,13 @@
+//
+//  EGViewController.h
+//  Ex01 - ToDoList
+//
+//  Created by Eddie Gurnee on 3/11/14.
+//  Copyright (c) 2014 Eddie Gurnee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EGViewController : UIViewController
+
+@end
---
+++ Exams/Exam1_EddieGurnee/Ex01 - ToDoList/Ex01 - ToDoListTests/Ex01___ToDoListTests.m
@@ -0,0 +1,34 @@
+//
+//  Ex01___ToDoListTests.m
+//  Ex01 - ToDoListTests
+//
+//  Created by Eddie Gurnee on 3/11/14.
+//  Copyright (c) 2014 Eddie Gurnee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ex01___ToDoListTests : XCTestCase
+
+@end
+
+@implementation Ex01___ToDoListTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
