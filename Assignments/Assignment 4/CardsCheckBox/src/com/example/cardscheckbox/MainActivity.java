80daaf1384742fc40d9bc951eefa3f7942ee7717
Eddie Gurnee <egurnee@emich.edu>
2014-03-25 19:10:03 -0400

    checkbox listener

--- Assignments/Assignment 4/CardsCheckBox/src/com/example/cardscheckbox/MainActivity.java
+++ Assignments/Assignment 4/CardsCheckBox/src/com/example/cardscheckbox/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.cardscheckbox;
 
-import android.os.Bundle;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+@TargetApi(Build.VERSION_CODES.HONEYCOMB) public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        ((CheckBox)this.findViewById(R.id.c_chbx)).setOnCheckedChangeListener(new CheckBoxListener());
+        ((CheckBox)this.findViewById(R.id.d_chbx)).setOnCheckedChangeListener(new CheckBoxListener());
+        ((CheckBox)this.findViewById(R.id.h_chbx)).setOnCheckedChangeListener(new CheckBoxListener());
+        ((CheckBox)this.findViewById(R.id.s_chbx)).setOnCheckedChangeListener(new CheckBoxListener());
+        
+        this.findViewById(R.id.hide_btn).setOnClickListener(new ButtonListener());
+        this.findViewById(R.id.show_btn).setOnClickListener(new ButtonListener());
     }
 
 
@@ -20,6 +36,37 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    private class CheckBoxListener implements OnCheckedChangeListener {
+
+		@Override
+		public void onCheckedChanged(CompoundButton theBox,
+				boolean isChecked) {
+			switch (theBox.getId()) {
+			case R.id.c_chbx:
+				((ImageView)MainActivity.this.findViewById(R.id.c_img)).setAlpha(isChecked ? 1.0f : .3f);
+				break;
+			case R.id.d_chbx:
+				((ImageView)MainActivity.this.findViewById(R.id.d_img)).setAlpha(isChecked ? 1.0f : .3f);
+				break;
+			case R.id.h_chbx:
+				((ImageView)MainActivity.this.findViewById(R.id.h_img)).setAlpha(isChecked ? 1.0f : .3f);
+				break;
+			case R.id.s_chbx:
+				((ImageView)MainActivity.this.findViewById(R.id.s_img)).setAlpha(isChecked ? 1.0f : .3f);
+				break;
+			}
+		}
+    	
+    }
     
+    private class ButtonListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			
+			
+		}
+    	
+    }
     
 }
